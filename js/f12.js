aae53200eecc0d048f9e97f38c690dc8a3410120
SNTube <qq869865681@gmail.com>
2023-09-25 18:32:47 +0800

    Site updated: 2023-09-25 18:32:45

--- js/f12.js
+++ js/f12.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", function(e) {
     if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.keyCode === 73)) {
         e.preventDefault();
-        window.location = "https://www.bing.com/search?q=%E6%80%8E%E6%A0%B7%E5%AD%A6%E4%B9%A0js%E3%80%81html%E5%92%8Ccss%EF%BC%9F";
+        window.location = "/page/stardust/";
     }
 });
