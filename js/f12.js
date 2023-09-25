a5288678f056e9f26b7db80ffcd476cb71f07e21
SNTube <qq869865681@gmail.com>
2023-09-25 21:14:03 +0800

    Site updated: 2023-09-25 21:14:01

--- js/f12.js
+++ js/f12.js
@@ -1,6 +1,6 @@
 window.addEventListener("keydown", function(e) {
     if (e.key === "F12" || (e.ctrlKey && e.shiftKey && e.keyCode === 73)) {
         e.preventDefault();
-        window.location = "/page/stardust/";
+        window.location = "/page/particlewave/";
     }
 });
