9151ac0cecd7b367607405f7fb929f8ec1ebe14a
SNTube <qq869865681@gmail.com>
2023-09-26 18:10:46 +0800

    Site updated: 2023-09-26 18:10:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/HelloWorld/","posts/d50a/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/HelloWorld/","posts/af320382/","posts/d50a/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
