77baa5b967c23c373e76edf41f5ac76680935c51
SNTube <qq869865681@gmail.com>
2023-10-20 15:51:07 +0800

    Site updated: 2023-10-20 15:51:02

--- js/scrolldisplay.js
+++ js/scrolldisplay.js
@@ -17,5 +17,5 @@ function hideScrollbar() {
 
 function showScrollbar() {
   document.documentElement.style.setProperty('--scrollbar-color-thumb', 'repeating-linear-gradient(45deg, #f771e8, #f7c6f2 5px, #21809c 5px, #21809c 10px, #f7c6f2 10px, #f771e8 15px)');
-  document.documentElement.style.setProperty('--global-color-thumb', 'darken(#f771e8, 2)');
+  document.documentElement.style.setProperty('--global-color-thumb', '#f771e8');
 }  
